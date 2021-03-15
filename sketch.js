07a474b0809af91f76d2776e229a5847eac6081d
AadiGitHubCoding <77033778+AadiGitHubCoding@users.noreply.github.com>
2021-03-15 12:25:23 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+   world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+
+
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+
+
+    box5 = new Box(810,160,70,70);
+    
+    bird = new Bird(200,50);
+
+  
+}
+
+function draw(){
+   
+   // Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    
+
+    box3.display();
+    box4.display();
+   
+
+    box5.display();
+    
+
+    bird.display();
+       
+}
+
